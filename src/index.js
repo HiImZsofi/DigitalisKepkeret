aaed0f33400b2489ec0d848a5b40761f36f99ae8
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-28 12:29:52 +0200

    main page basic design

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle.js"
 
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
