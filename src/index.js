a7aab1b526641fef3ff0077cc6a1ce8576dd74fb
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-28 12:45:05 +0200

    img append and set css to given number

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.js"
 
 $(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
+    $( "#generate" ).click(function() {
+        var urlvalue = $( "#url" ).val();  //get url value
+        var imgwidth = parseInt($("#width").val()); //get width value and parse it into a number
+        $('.picture').attr("src", urlvalue);
+        $(".picture").css('width', imgwidth);
+      });
 });
