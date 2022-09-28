2d4a3c69b9a1f13ae5409380e31fc5523e1c3bac
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-28 13:59:45 +0200

    removing button and cleaning up code

--- src/index.js
+++ src/index.js
@@ -9,19 +9,6 @@ var colorvalue = 0;
 var bordervalue = 0;
 
 $(document).ready(function() {
-    // $( "#generate" ).click(function() {
-    //     var urlvalue = $( "#url" ).val();  //get url value
-    //     var imgwidth = parseInt($("#width").val()); //get width value and parse it into a number
-    //     var bordervalue = parseInt($("#borderthickness").val());  //get border thickness value and parse it into a number
-    //     var colorvalue = $( "#bordercolor" ).val();
-    //     $('.picture').attr("src", urlvalue); //img append to specific class
-    //     $(".picture").css('width', imgwidth); //set the css to given number
-    //     $('.picture').css('border', "solid " + bordervalue + "px " + colorvalue);  
-    //     $('.picture').css('margin', "auto");  
-    //     $('.picture').css('display', "block");  //basic image styling after attr
-    //     $('.picture').css('margin-top', "20px");  
-    //   });
-
       $( "#url" ).change(function() {
         urlvalue = $( "#url" ).val();  //get url value
         $('.picture').attr("src", urlvalue); //img append to specific class
