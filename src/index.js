7d83c8f655d663f7eb902d254f9a425cd0721035
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-28 12:52:49 +0200

    set border dynamically

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
     $( "#generate" ).click(function() {
         var urlvalue = $( "#url" ).val();  //get url value
         var imgwidth = parseInt($("#width").val()); //get width value and parse it into a number
-        $('.picture').attr("src", urlvalue);
-        $(".picture").css('width', imgwidth);
+        var bordervalue = parseInt($("#borderthickness").val());  //get border thickness value and parse it into a number
+        $('.picture').attr("src", urlvalue); //img append to specific class
+        $(".picture").css('width', imgwidth); //set the css to given number
+        $('.picture').css('border', "solid " + bordervalue + "px red");  
       });
 });
